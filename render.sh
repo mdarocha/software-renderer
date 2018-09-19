8f6cb8c44d1344d30741a2304a3bc602a531dd4a
mdarocha <11572618+mdarocha@users.noreply.github.com>
2018-09-19 17:05:51 +0200

    Change CMake structure, add tests folder

--- render.sh
+++ render.sh
@@ -3,6 +3,6 @@ cd build/
 rm softrender
 rm test.ppm
 make
-./softrender ../teapot.obj test.ppm 2000 2000
+./src/softrender ../teapot.obj test.ppm 2000 2000
 cd ..
 feh --force-aliasing build/test.ppm
